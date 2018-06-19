ea1d4cf8ed4b5c544e35fa732046b92c1e0b9e7e
Soufian AIT TIRITE <aittirite.soufian@gmail.com>
2018-06-19 18:34:04 +0200

    init

---
+++ database/migrations/2018_05_30_124459_create_annonces_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnnoncesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('annonces', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('description');
+            $table->double('price');
+            $table->string('image');
+            $table->integer('user')->unsigned();
+            $table->integer('category')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('user')->references('id')->on('users');
+            $table->foreign('category')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('annonces');
+    }
+}
---
+++ database/migrations/2018_05_30_132710_create_rating_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRatingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rating', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('rate');
+            $table->integer('user')->unsigned();
+            $table->timestamps();
+            
+            $table->foreign('user')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rating');
+    }
+}
