f01a269dc0d0d8f9db68aa89049e1b120e112be1
Soufian AIT TIRITE <aittirite.soufian@gmail.com>
2018-06-21 23:24:17 +0200

    update

--- app/Annonce.php
+++ app/Annonce.php
@@ -7,12 +7,15 @@ use Auth;
 
 class Annonce extends Model
 {
-    public function category(){
+	public function category(){
 		return $this->belongsTo(Category::class);
 	}
 
-	/*public function user(){
-        return $this->belongsToOne('App\User');
-//		return User::where('id', Auth::user()->id)->get();
-	}*/
+	public function user(){
+		return $this->belongsTo(User::class, Auth::user()->id);
+	}
+
+	public function userList(){
+		return User::where('id', Auth::user()->id)->get();
+	}
 }
