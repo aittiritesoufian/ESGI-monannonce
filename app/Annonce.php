5e3e5c800ba8a99656f2c692f3106d25289049e0
Soufian AIT TIRITE <aittirite.soufian@gmail.com>
2018-06-21 22:57:31 +0200

    ajout de réglages

--- app/Annonce.php
+++ app/Annonce.php
@@ -3,8 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Auth;
 
 class Annonce extends Model
 {
-    //
+    public function category(){
+		return $this->belongsTo(Category::class);
+	}
+
+	/*public function user(){
+        return $this->belongsToOne('App\User');
+//		return User::where('id', Auth::user()->id)->get();
+	}*/
 }
