ae2ff5829b6e62c645c158239151164c8ff34877
squadzero <theosen95@gmail.com>
2018-06-22 11:28:20 +0200

    annonce controller

--- app/Http/Controllers/annonceController.php
+++ app/Http/Controllers/annonceController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 use Illuminate\Routing\Controller as BaseController;
 use App\Annonce;
+use Auth;
 
 class annonceController extends Controller {
 	public function show($id = "")
 	{
 	   	$post = Annonce::whereRaw('id = ?', [$id])->get();	
-	   	return view('annonce', ['id' => $id, "annonce" => $post]);
+	   	$currentID = Auth::user()->id;
+	   	return view('annonce', ['id' => $id, "annonce" => $post[0], 'currentUserId' => $currentID]);
 	}
 }
