d5423c39a6cf1e589362fe1731eb20d74a1d4b1a
pierret <lancewinnfield@gmail.com>
2018-06-22 11:13:12 +0200

    integration dynamique

--- resources/views/templates/global.blade.php
+++ resources/views/templates/global.blade.php
@@ -38,13 +38,13 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#">LeCoinBon</a>
+				<a class="navbar-brand" href="./accueil">weshAnnonce</a>
 			</div>
 
 			<!-- Collect the nav links, forms, and other content for toggling -->
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
-					<li class="active"><a href="./accueil">Accueil</a></li>
+					<li class="active"><a href="./accueil">weshHome</a></li>
 					<li><a href="./annonces">Consulte les annonces</a></li>
 				</ul>
 				
